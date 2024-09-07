576b56f22d6d6db8b703918cb049be95af80645c
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-09-07 21:47:03 +0600

    Redirecting welcome page to login page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/login');
 });
 
 Auth::routes();
