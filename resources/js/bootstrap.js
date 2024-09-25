c81f6ea61524a56429b486ac10c394924ccc6472
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-09-25 17:40:51 +0600

    Adding axios common header at bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 /* Token */
 window.axios.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(window.localStorage.getItem('access_token'))}`;
