6621cbb1a28d00494d8efd56da8b743d02b93a8e
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-10-11 02:48:49 +0600

    Factory & Seeder for VaccineCenter added

---
+++ database/factories/VaccineCenterFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class VaccineCenterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text(6)."-".fake()->unique()->numberBetween($min = 1, $max = 999),
+            'maximum_vaccine_per_day' => fake()->randomDigitNotNull(),
+        ];
+    }
+}
