82a0f36b07c5817181e59e7f0a3ee876c7c1f7a3
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-10-11 22:18:01 +0600

    UserSeeder is added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
             VaccineCenterSeeder::class,
             AdminSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
